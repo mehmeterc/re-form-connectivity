a1b650a278f98ef10d6077faa57183c250ec311e
Mehmet Ercan <48414528+mehmeterc@users.noreply.github.com>
2025-03-20 22:50:49 +0100

    submit2

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -121,7 +121,7 @@ const Contact = () => {
                 />
                 <Button 
                   type="submit" 
-                  className="ml-2 bg-black text-white hover:bg-black/90 dark:bg-white dark:text-black dark:hover:bg-white/90"
+                  className="ml-2 bg-black text-white hover:bg-black/90 dark:bg-black dark:text-black dark:hover:bg-black/90"
                 >
                   <Send className="h-4 w-4 text-white" />
                 </Button>
